59fe368b05166a8eb303974958bcdcf5e9196051
Daniel <daniel_gileta@hotmail.com>
2014-01-26 21:56:03 -0600

    Branch Local Paso 2 -Daniel

--- hola.c
+++ hola.c
@@ -4,13 +4,14 @@ void main(void) {
 	printf("Hola Mundo\n");
 }
 
-
-void funcion2(void){
-	printf("funcion 2\n)
+void funcion2(void) {
+	printf("Funcion 2\n")
 }
-
 
 void funcion1(void) {
 	printf("Funcion 1\n");
 }
 
+void funcion3(void) {
+	printf("Funcion 3\n");
+}
